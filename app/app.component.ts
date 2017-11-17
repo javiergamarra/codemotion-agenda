6b1ce3741e5f3a5727043645d10c9b88e49827b8
Javier Gamarra <nhpatt@gmail.com>
2017-11-17 15:24:18 +0100

    tabs and talks

--- app/app.component.ts
+++ app/app.component.ts
@@ -4,30 +4,64 @@ import { TalksService } from './talks/talks.service';
 @Component({
     selector: 'my-app',
     template: `
+
 		<ActionBar title="My App" class="action-bar"></ActionBar>
 
-		<StackLayout orientation="vertical" *ngFor="let day of talks$ | async">
 
-			<Label text='{{day.name}}'></Label>
+		<Label text="{{days | json}}"></Label>
 
-			<StackLayout *ngFor="let track of day.tracks">
-				<Label text='{{track.name}}'></Label>
-				<StackLayout *ngFor="let slot of track?.slots">
-					<Label text='{{slot.contents.title}}'></Label>
+		<ng-container *ngIf="days && days">
+			<TabView selectedIndex="0"
+			         sdkExampleTitle sdkToggleNavButton>
+				<!--<ng-container *ngFor="let day of talks$ | async">-->
+
+				<Label text="{{days | json}}"></Label>
+                
+				<StackLayout *tabItem="{title: days[0].name}">
+					<ScrollView>
+						<StackLayout>
+							<Label text="{{days[0].name}}" textWrap="true" class="m-15"></Label>
+							<ng-container *ngFor="let track of days[0].tracks">
+								<Label text="{{track.name}}" textWrap="true" class="m-15"></Label>
+								<ng-container *ngFor="let slot of track?.slots">
+									<Label text='{{slot.contents.title}}'></Label>
+								</ng-container>
+							</ng-container>
+						</StackLayout>
+					</ScrollView>
 				</StackLayout>
-			</StackLayout>
-		</StackLayout>
+				<StackLayout *tabItem="{title: days[1].name}">
+					<ScrollView>
+						<StackLayout>
+							<Label text="{{days[1].name}}" textWrap="true" class="m-15"></Label>
+							<ng-container *ngFor="let track of days[1].tracks">
+								<Label text="{{track.name}}" textWrap="true" class="m-15"></Label>
+								<ng-container *ngFor="let slot of track?.slots">
+									<Label text='{{slot.contents.title}}'></Label>
+								</ng-container>
+							</ng-container>
+						</StackLayout>
+					</ScrollView>
+				</StackLayout>
+				<!--</ng-container>-->
+			</TabView>
+		</ng-container>
     `
 })
 export class AppComponent implements OnInit {
-    private talks$: any;
+
+    private days: any;
 
     constructor(private talksService: TalksService) {
 
     }
 
     ngOnInit(): void {
-        this.talks$ = this.talksService.getAllTalks().map(x => x['days']);
+        this.talksService.getAllTalks().map(x => x['days']).subscribe(days => this.days = days);
     }
 
 }
+
+//
+
+// (selectedIndexChange)="onIndexChanged($event)"
