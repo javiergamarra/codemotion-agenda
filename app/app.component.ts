1efef2964dd63c7ab88ba8f88fe68b4bcebd7745
Javier Gamarra <nhpatt@gmail.com>
2017-11-17 19:37:00 +0100

    look ordered by hour

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,61 +7,41 @@ import { TalksService } from './talks/talks.service';
 
 		<ActionBar title="My App" class="action-bar"></ActionBar>
 
-
-		<Label text="{{days | json}}"></Label>
-
-		<ng-container *ngIf="days && days">
-			<TabView selectedIndex="0"
-			         sdkExampleTitle sdkToggleNavButton>
-				<!--<ng-container *ngFor="let day of talks$ | async">-->
-
-				<Label text="{{days | json}}"></Label>
-                
-				<StackLayout *tabItem="{title: days[0].name}">
-					<ScrollView>
-						<StackLayout>
-							<Label text="{{days[0].name}}" textWrap="true" class="m-15"></Label>
-							<ng-container *ngFor="let track of days[0].tracks">
-								<Label text="{{track.name}}" textWrap="true" class="m-15"></Label>
-								<ng-container *ngFor="let slot of track?.slots">
-									<Label text='{{slot.contents.title}}'></Label>
-								</ng-container>
-							</ng-container>
-						</StackLayout>
-					</ScrollView>
-				</StackLayout>
-				<StackLayout *tabItem="{title: days[1].name}">
-					<ScrollView>
-						<StackLayout>
-							<Label text="{{days[1].name}}" textWrap="true" class="m-15"></Label>
-							<ng-container *ngFor="let track of days[1].tracks">
-								<Label text="{{track.name}}" textWrap="true" class="m-15"></Label>
-								<ng-container *ngFor="let slot of track?.slots">
-									<Label text='{{slot.contents.title}}'></Label>
-								</ng-container>
-							</ng-container>
-						</StackLayout>
-					</ScrollView>
-				</StackLayout>
-				<!--</ng-container>-->
+		<ng-container *ngIf="days$ | async; let days">
+			<TabView selectedIndex="0" sdkExampleTitle sdkToggleNavButton>
+				<ng-container *ngFor="let day of days">
+					<StackLayout *tabItem="{title: day.name}">
+						<ScrollView>
+							<StackLayout>
+								<StackLayout *ngFor="let hour of keys(day.hours)">
+									<Label text="{{hour}}" class="m-15"></Label>
+									<Label text="{{slot.contents.title}}" *ngFor="let slot of day.hours.get(hour)"
+									       class="m-25"></Label>
+								</StackLayout>
+							</StackLayout>
+						</ScrollView>
+					</StackLayout>
+				</ng-container>
 			</TabView>
 		</ng-container>
     `
 })
 export class AppComponent implements OnInit {
 
-    private days: any;
+    private days$: any;
 
     constructor(private talksService: TalksService) {
 
     }
 
+    keys(map) {
+        return Array.from(map.keys());
+    }
+
     ngOnInit(): void {
-        this.talksService.getAllTalks().map(x => x['days']).subscribe(days => this.days = days);
+        this.days$ = this.talksService.getAllTalks();
     }
 
 }
-
-//
 
 // (selectedIndexChange)="onIndexChanged($event)"
--- app/talks/talks.service.ts
+++ app/talks/talks.service.ts
@@ -11,6 +11,25 @@ export class TalksService {
 
     getAllTalks() {
         return this.http.get('https://www.koliseo.com/codemotion/codemotion-madrid/r4p/5632002325741568/agenda')
-            .map(x => x.json());
+            .map(x => x.json())
+            .map(x => x['days'])
+            .map(days => {
+
+                for (let day of days) {
+                    let hours = new Map();
+
+                    for (let track of day.tracks) {
+                        for (let slot of track.slots) {
+                            if (slot.contents.title) {
+                                let value = hours.has(slot.start) ? hours.get(slot.start) : [];
+                                value.push(slot);
+                                hours.set(slot.start, value);
+                            }
+                        }
+                    }
+                    day.hours = hours;
+                }
+                return days;
+            });
     }
 }
