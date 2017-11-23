704c85111bbd93879ab7d5e77d4bd93e2afc40f5
Sarai Díaz García <dgarciasarai@gmail.com>
2017-11-23 01:18:52 +0100

    move template to another file

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,29 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { TalksService } from './talks/talks.service';
 
 @Component({
-    selector: 'my-app',
-    template: `
-
-		<ActionBar title="My App" class="action-bar"></ActionBar>
-
-		<ng-container *ngIf="days$ | async; let days">
-			<TabView selectedIndex="0" sdkExampleTitle sdkToggleNavButton>
-				<ng-container *ngFor="let day of days">
-					<StackLayout *tabItem="{title: day.name}">
-						<ScrollView>
-							<StackLayout>
-								<StackLayout *ngFor="let hour of keys(day.hours)">
-									<Label text="{{hour}}" class="m-15"></Label>
-									<Label text="{{slot.contents.title}}" *ngFor="let slot of day.hours.get(hour)"
-									       class="m-25"></Label>
-								</StackLayout>
-							</StackLayout>
-						</ScrollView>
-					</StackLayout>
-				</ng-container>
-			</TabView>
-		</ng-container>
-    `
+	selector: 'my-app',
+	templateUrl: "./talks/talks.component.html"
 })
 export class AppComponent implements OnInit {
 
